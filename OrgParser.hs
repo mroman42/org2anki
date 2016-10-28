4459fc1e9319a5ec634b95110bf60c05e5889472
M42 <mromang08@gmail.com>
2016-10-28 23:59:35 +0200

    Commenting code

--- OrgParser.hs
+++ OrgParser.hs
@@ -1,3 +1,13 @@
+{-|
+Module: OrgParser
+Author: Mario Román
+Description: A parser of the markup-related subset of org-mode
+License: GPL-3
+
+This module implements a set of parsers of the markup syntax of org-mode.
+It is not a complete implementation of a org-mode parser. It only parses
+certain syntax constructs mostly related to its internal markdown language.
+-}
 module OrgParser
   ( OrgHeader(..)
   , OrgTree(..)
@@ -17,41 +27,45 @@ instance Eq OrgHeader where
 instance Ord OrgHeader where
   compare (OrgHeader a _ _) (OrgHeader b _ _) = compare a b
 
-
+-- | Parses end of line or end of file
 eol :: Parser ()
 eol = ((char '\n') >> return ()) <|> eof
 
+-- | Parses a line that is NOT a header
 textline :: Parser String
 textline = notFollowedBy (char '*') >> many (noneOf "\n")
 
+-- | Parses a set of lines, where none of them are headers
 text :: Parser String
 text = unlines <$> sepEndBy (try textline) (try $ char '\n')
 
+-- | Parses an org-mode header
 header :: Parser OrgHeader
 header = OrgHeader
   <$> asts
   <*> ((char ' ') >> manyTill anyChar eol)
   <*> text
 
+-- | Counts asterisks
 asts :: Parser Int
 asts = length <$> many1 (try (char '*'))
 
+-- | Parses a complete org-file, dividing it in headers
 orgfile :: Parser [OrgHeader]
 orgfile = many header
 
-
-data OrgTree = OrgTree OrgHeader [OrgTree]
-
+-- | Given a list of headers, structures it into a tree, where
+-- every header has a list of child headers.
 struct :: [OrgHeader] -> [OrgTree]
 struct [] = []
 struct (h:hs) = OrgTree h (struct internal) : (struct reminder)
   where (internal,reminder) = break (<=h) hs
 
+-- | Parses a complete org-file, giving it tree structure
 orgtree :: Parser [OrgTree]
 orgtree = struct <$> orgfile
 
-instance Show OrgTree where
-  show = showTree
+data OrgTree = OrgTree OrgHeader [OrgTree]
 
 showTree :: OrgTree -> String
 showTree (OrgTree head trees) = unlines $ map ("| " ++) $ lines $ unlines
@@ -60,3 +74,6 @@ showTree (OrgTree head trees) = unlines $ map ("| " ++) $ lines $ unlines
   , content head
   , concatMap showTree trees
   ]
+
+instance Show OrgTree where
+  show = showTree
